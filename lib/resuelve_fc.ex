78666be49605cc55f07a7e8ab114c5d19fcb5b5b
Saul Perez <saul.perez@bunsan.io>
2023-07-06 12:06:58 -0600

    🔧 Added typedstruct specifications

--- lib/resuelve_fc.ex
+++ lib/resuelve_fc.ex
@@ -2,6 +2,40 @@ defmodule ResuelveFc do
   @moduledoc """
   Calculate Salaries and Bonus for `ResuelveFc`.
   """
+  use TypedStruct
+  %{
+    "bono" => 25000,
+    "equipo" => "rojo",
+    "goles" => 10,
+    "nivel" => "C",
+    "nombre" => "Juan Perez",
+    "sueldo" => 50000,
+    "sueldo_completo" => nil
+  }
+
+  typedstruct module: Player do
+    field(:bono, Integer.t())
+    field(:equipo, String.t())
+    field(:goles, Integer.t())
+    field(:nivel, String.t())
+    field(:nombre, String.t())
+    field(:sueldo, Integer.t())
+    field(:sueldo_completo, nil)
+  end
+
+  typedstruct module: PlayerWithFullSalary do
+    field(:bono, Integer.t())
+    field(:equipo, String.t())
+    field(:goles, Integer.t())
+    field(:nivel, String.t())
+    field(:nombre, String.t())
+    field(:sueldo, Integer.t())
+    field(:sueldo_completo, Integer.t())
+  end
+
+
+
+
   @minimum_goals_per_level %{
     "A" => %{"rojo" => 5, "azul" => 6, "verde" => 7},
     "B" => %{"rojo" => 10, "azul" => 11, "verde" => 12},
@@ -9,7 +43,7 @@ defmodule ResuelveFc do
     "Cuauh" => %{"rojo" => 20, "azul" => 21, "verde" => 22}
   }
 
-  def calculate_bonus(player) do
+  defp calculate_bonus(player) do
     individual_goal_achievement =
       case player["nivel"] do
         level when is_integer(level) ->
@@ -33,6 +67,7 @@ defmodule ResuelveFc do
     player |> Map.put("sueldo_completo", player["sueldo"] + total_bonus)
   end
 
+  @spec calculate_salaries(list(Player.t())) :: list(PlayerWithFullSalary.t())
   def calculate_salaries(players) do
     Enum.map(players, &calculate_bonus/1)
   end
