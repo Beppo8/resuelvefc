dc8a48fc33438da3930b554d71cb57c7cdc17960
Saul Perez <saul.perez@bunsan.io>
2023-07-06 10:56:11 -0600

    WIP: Calculate Salaries for ResuelveFC

--- lib/resuelve_fc.ex
+++ lib/resuelve_fc.ex
@@ -1,18 +1,34 @@
 defmodule ResuelveFc do
   @moduledoc """
-  Documentation for `ResuelveFc`.
+  Calculate Salaries and Bonus for `ResuelveFc`.
   """
+  @minimum_goals_per_level %{
+    "A" => 5,
+    "B" => 10,
+    "C" => 15,
+    "Cuauh" => 20
+  }
 
-  @doc """
-  Hello world.
+  def calculate_bonus(player) do
+    individual_goal_achievement =
+      case player["nivel"] do
+        level when is_integer(level) -> player["goles"] / @minimum_goals_per_level[level]
+        _ -> 0
+      end
 
-  ## Examples
+    team_goal_achievement =
+      Enum.reduce([player["equipo"]], 0, fn _, acc ->
+        acc + Map.get(player, "goles", 0)
+      end) / 50
 
-      iex> ResuelveFc.hello()
-      :world
+    individual_bonus = player["bono"] * individual_goal_achievement / 100
+    team_bonus = player["bono"] * team_goal_achievement / 100
+    total_bonus = individual_bonus + team_bonus
 
-  """
-  def hello do
-    :world
+    player |> Map.put("sueldo_completo", player["sueldo"] + total_bonus)
+  end
+
+  def calculate_salaries(players) do
+    Enum.map(players, &calculate_bonus/1)
   end
 end
--- test/resuelve_fc_test.exs
+++ test/resuelve_fc_test.exs
@@ -1,8 +1,91 @@
 defmodule ResuelveFcTest do
   use ExUnit.Case
-  doctest ResuelveFc
+  alias ResuelveFc
 
-  test "greets the world" do
-    assert ResuelveFc.hello() == :world
+  describe "ResuelveFc test" do
+    test "When receive only from one team should return excpected result" do
+      input_json = %{
+        "jugadores" => [
+          %{
+            "bono" => 25000,
+            "equipo" => "rojo",
+            "goles" => 10,
+            "nivel" => "C",
+            "nombre" => "Juan Perez",
+            "sueldo" => 50000,
+            "sueldo_completo" => nil
+          },
+          %{
+            "bono" => 30000,
+            "equipo" => "azul",
+            "goles" => 30,
+            "nivel" => "Cuauh",
+            "nombre" => "EL Cuauh",
+            "sueldo" => 100_000,
+            "sueldo_completo" => nil
+          },
+          %{
+            "bono" => 10000,
+            "equipo" => "azul",
+            "goles" => 7,
+            "nivel" => "A",
+            "nombre" => "Cosme Fulanito",
+            "sueldo" => 20000,
+            "sueldo_completo" => nil
+          },
+          %{
+            "bono" => 15000,
+            "equipo" => "rojo",
+            "goles" => 9,
+            "nivel" => "B",
+            "nombre" => "El Rulo",
+            "sueldo" => 30000,
+            "sueldo_completo" => nil
+          }
+        ]
+      }
+
+      output = [
+        %{
+          "bono" => 25000,
+          "equipo" => "rojo",
+          "goles" => 10,
+          "nivel" => "C",
+          "nombre" => "Juan Perez",
+          "sueldo" => 50000,
+          "sueldo_completo" => 50050.0
+        },
+        %{
+          "bono" => 30000,
+          "equipo" => "azul",
+          "goles" => 30,
+          "nivel" => "Cuauh",
+          "nombre" => "EL Cuauh",
+          "sueldo" => 100_000,
+          "sueldo_completo" => 100_180.0
+        },
+        %{
+          "bono" => 10000,
+          "equipo" => "azul",
+          "goles" => 7,
+          "nivel" => "A",
+          "nombre" => "Cosme Fulanito",
+          "sueldo" => 20000,
+          "sueldo_completo" => 20014.0
+        },
+        %{
+          "bono" => 15000,
+          "equipo" => "rojo",
+          "goles" => 9,
+          "nivel" => "B",
+          "nombre" => "El Rulo",
+          "sueldo" => 30000,
+          "sueldo_completo" => 30027.0
+        }
+      ]
+
+      result = ResuelveFc.calculate_salaries(input_json["jugadores"])
+      assert result == output
+    end
   end
 end
